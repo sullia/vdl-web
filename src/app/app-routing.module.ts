a4cc9be065a93615cfdfa7bf7f72f42729346444
visvmal7 <30434371+visvmal7@users.noreply.github.com>
2019-12-06 17:16:21 +0530

    Update on Currency

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { TruncationComponent } from './truncation/truncation.component';
 import { CorruptionComponent } from './corruption/corruption.component';
 import { DatetimeComponent } from './datetime/datetime.component';
 import { OverlappingComponent } from './overlapping/overlapping.component';
+import { CurrencyComponent } from './currency/currency.component';
 import { SupportComponent } from './support/support.component';
 
 const routes: Routes = [
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'corruption', component: CorruptionComponent },
   { path: 'datetime', component: DatetimeComponent },
   { path: 'overlapping', component: OverlappingComponent },
+  { path: 'currency', component: CurrencyComponent },
   { path: 'support', component: SupportComponent}
 ];
 
